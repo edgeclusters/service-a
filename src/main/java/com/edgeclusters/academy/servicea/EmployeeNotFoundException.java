deb8f95eac55c147349186e0847af858e800210b
amoravek <amoravek@gmail.com>
2022-01-20 00:49:29 +0100

    updates

---
+++ src/main/java/com/edgeclusters/academy/servicea/EmployeeNotFoundException.java
@@ -0,0 +1,10 @@
+package com.edgeclusters.academy.servicea;
+
+class EmployeeNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	EmployeeNotFoundException(Long id) {
+		super("Could not find employee " + id);
+	}
+}

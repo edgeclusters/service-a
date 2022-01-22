eb69dfb5c9a788b15c135d5e4bf28097252fdb3f
amoravek <amoravek@gmail.com>
2022-01-23 00:45:37 +0100

    updates

--- src/main/java/com/edgeclusters/academy/servicea/EmployeeController.java
+++ src/main/java/com/edgeclusters/academy/servicea/EmployeeController.java
@@ -2,6 +2,8 @@ package com.edgeclusters.academy.servicea;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,8 @@ import io.micrometer.core.annotation.Timed;
 @RestController
 class EmployeeController {
 
+	private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+
 	private final EmployeeRepository repository;
 
 	EmployeeController(EmployeeRepository repository) {
@@ -24,6 +28,7 @@ class EmployeeController {
 	@Timed(value = "employees.all.time", description = "Time taken to get all employees")
 	@GetMapping("/employees")
 	List<Employee> all() {
+		logger.info("GET all employees");
 		return repository.findAll();
 	}
 	// end::get-aggregate-root[]
@@ -31,14 +36,14 @@ class EmployeeController {
 	@Timed(value = "employees.create.time", description = "Time taken to create new employee")
 	@PostMapping("/employees")
 	Employee newEmployee(@RequestBody Employee newEmployee) {
+		logger.info("POST employee: " + newEmployee);
 		return repository.save(newEmployee);
 	}
-
-	// Single item
 
 	@Timed(value = "employee.get.time", description = "Time taken to get a single employee")
 	@GetMapping("/employees/{id}")
 	Employee one(@PathVariable Long id) {
+		logger.info("GET employee: " + id);
 		return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
 	}
 
@@ -46,6 +51,7 @@ class EmployeeController {
 	@PutMapping("/employees/{id}")
 	Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
 
+		logger.info("PUT employee id: " + id);
 		return repository.findById(id).map(employee -> {
 			employee.setName(newEmployee.getName());
 			employee.setRole(newEmployee.getRole());
@@ -59,6 +65,7 @@ class EmployeeController {
 	@Timed(value = "employee.delete.time", description = "Time taken to get a single employee by ID")
 	@DeleteMapping("/employees/{id}")
 	void deleteEmployee(@PathVariable Long id) {
+		logger.info("DELETE employe id: " + id);
 		repository.deleteById(id);
 	}
 }
